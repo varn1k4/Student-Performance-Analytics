230e9ced29c5472be7f6c1b12ec3f0e997af5193
varn1k4 <varnika.vads@gmail.com>
2026-06-19 10:45:11 +0530

    Add files via upload

---
+++ src/components/Login.jsx
@@ -0,0 +1,12 @@
+function Login() {
+  return (
+    <div>
+      <h2>Login Module</h2>
+      <input type="email" placeholder="Email" />
+      <input type="password" placeholder="Password" />
+      <button>Login</button>
+    </div>
+  );
+}
+
+export default Login;
---
+++ src/components/Students.jsx
@@ -0,0 +1,10 @@
+function Students() {
+  return (
+    <div>
+      <h2>Student Management</h2>
+      <button>Add Student</button>
+    </div>
+  );
+}
+
+export default Students;
